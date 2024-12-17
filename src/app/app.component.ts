ed68701d46cc804195c929cd6f3d65256f221fde
Rudraksh Vats <vatsrudraksh@gmail.com>
2024-12-17 16:22:39 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'todo';
+  message =
+    'Welcome to my page!'; /* This variable is used inside app.component.html for interpolation */
+}
