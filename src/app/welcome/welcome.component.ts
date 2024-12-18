4c85976e99c125ba55dbf9a67369d98fda893104
Rudraksh Vats <vatsrudraksh@gmail.com>
2024-12-18 12:43:13 +0530

    login component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor() { }
+  message = 'Some Welcome Message'; // without declaring the datatype typescript knows that this message is string
+  constructor() {}
 
   ngOnInit(): void {
+    // this.message = 5; --> This will throw compilation error
+    console.log(this.message);
   }
-
 }
