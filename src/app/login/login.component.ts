8126de14cbb4af8e4686b00f28b9dadeba344d5d
Rudraksh Vats <vatsrudraksh@gmail.com>
2024-12-18 13:15:55 +0530

    event handling login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  username = 'Rudraksh';
+  password = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  // creating an action/event method on login button click
+  handleLogin() {
+    console.log(this.username);
   }
-
 }
