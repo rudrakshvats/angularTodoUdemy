af555a38760236e8e23b8ce80266feed266ecd86
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-20 11:18:53 +0530

    navigation by routing in angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,9 @@ export class LoginComponent implements OnInit {
   errorMessage = 'Invalid Credentials';
   invalidLogin = false;
 
-  constructor() {}
+  constructor(
+    private router: Router // here we are basically injecting the router component variable by the concept of dependency injection as we do in Java, note that dependency injection in one component for other component is done inside the constructor
+  ) {}
 
   ngOnInit(): void {}
 
@@ -19,6 +22,8 @@ export class LoginComponent implements OnInit {
   handleLogin() {
     console.log(this.username);
     if (this.username === 'Rudraksh' && this.password === 'dummy') {
+      // if user details are validated, then we will redirect the user to Login Page using routes
+      this.router.navigate(['welcome']); // we are telling the browser to navigate to welcome page whenever user validates
       this.invalidLogin = false;
     } else {
       this.invalidLogin = true;
