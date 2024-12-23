a50b0a72be580d9d372344eff33fb48b43807faa
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-23 16:01:00 +0530

    authentication checks and session storage

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -9,9 +9,17 @@ export class HardcodedAuthenticationService {
 
   authenticate(username: string, password: string) {
     // simply created a method for authentication for hardcoded values so that user can login according to it, the arguments/parameters can simply be used when we use it inside some other service
+    console.log('before', this.isUserLoggedIn());
     if (username === 'Rudraksh' && password === 'dummy') {
+      sessionStorage.setItem('authenticatedUser', username);
+      console.log('after', this.isUserLoggedIn());
       return true;
     }
     return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticatedUser');
+    return !(user === null);
   }
 }
