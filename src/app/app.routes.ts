7e905a1444e46f7948f465c39f930406f9063358
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-23 16:37:57 +0530

    router guard service and redirection to login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,21 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:name', component: WelcomeComponent }, // here we are telling browser that whatever parameter name is passed in the url then we will redirect that url to welcome page
-  { path: 'todos', component: ListTodosComponent },
+  {
+    path: 'welcome/:name',
+    component: WelcomeComponent,
+    canActivate: [RouteGuardService],
+  }, // here we are telling browser that whatever parameter name is passed in the url then we will redirect that url to welcome page
+  {
+    path: 'todos',
+    component: ListTodosComponent,
+    canActivate: [RouteGuardService],
+  },
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent }, // here ** means if any other part is entered than above paths then we will redirect to Error Page
 ];
--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
@@ -13,12 +14,15 @@ import { HardcodedAuthenticationService } from './hardcoded-authentication.servi
 })
 export class RouteGuardService implements CanActivate {
   constructor(
-    public hardcodedAuthenticationService: HardcodedAuthenticationService
+    public hardcodedAuthenticationService: HardcodedAuthenticationService,
+    public router: Router
   ) {}
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
       return true;
     }
+    this.router.navigate(['login']); // adding this line would redirect every user to login page if they are not logged in and try to return to any other page other than login
     return false;
   }
 }
