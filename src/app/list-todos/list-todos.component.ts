b42df98a201165886fdc4e9798e7fab8b2e659e6
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-23 11:40:55 +0530

    creating class inside typescript file and using Date in our ts and html file

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
+export class Todo {
+  // creating a class with parameters below and their datatypes
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {}
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +18,12 @@ import { DatePipe } from '@angular/common';
 })
 export class ListTodosComponent implements OnInit {
   todos = [
-    { id: 1, description: 'Learn Angular' },
-    { id: 2, description: 'Become good anngular developer' },
-    { id: 3, description: 'Visit Santiago Bernabeu Stadium in Madrid' },
+    new Todo(1, 'Learn Angular', false, new Date()), // declaring objects with parameterised contructor approach just like Java
+    new Todo(2, 'Become good anngular developer', false, new Date()),
+    new Todo(3, 'Visit Santiago Bernabeu Stadium in Madrid', false, new Date()),
+    // { id: 1, description: 'Learn Angular' },
+    // { id: 2, description: 'Become good anngular developer' },
+    // { id: 3, description: 'Visit Santiago Bernabeu Stadium in Madrid' },
   ];
   // todo = {
   //   id: 1,
