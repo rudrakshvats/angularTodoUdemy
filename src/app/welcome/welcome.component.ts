fcfe71d1c09087bee6d9732c41b15f0bb5985595
rudraksh.vats <rudraksh.vats@payu.in>
2024-12-23 10:48:17 +0530

    changes in welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-welcome',
